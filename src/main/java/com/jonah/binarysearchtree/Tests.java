16239298a08bd4ba6bbd2aea886566275733b6d2
Jonah-B-Kehillah <jbarnett2023@kehillahstudent.org>
2022-09-07 13:24:59 -0700

    Added stuff to testing file

--- src/main/java/com/jonah/binarysearchtree/Tests.java
+++ src/main/java/com/jonah/binarysearchtree/Tests.java
@@ -10,5 +10,14 @@ package com.jonah.binarysearchtree;
  * @author jonah
  */
 public class Tests {
-	
+	public static void main(String[] args){
+		
+		int passed = 0;
+		int failed = 0;
+		
+		// call test functions here
+		
+		System.out.println(String.format("Tests passed: %d, Tests failed: %d", passed, failed));
+		
+	}
 }
